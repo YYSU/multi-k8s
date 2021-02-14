64e3521aecba6101dd92051bd3e733730e27a3f8
YYSU <jim123820@gmail.com>
2021-02-14 11:54:10 +0000

    Modify travis ci yaml

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 docker build -t jim123820/multi-client:latest -t jim123820/multi-client:$SHA -f ./client/Dockerfile ./client
-# docker build -t jim123820/multi-server:latest -t jim123820/multi-server:$SHA -f ./server/Dockerfile ./server
-# docker build -t jim123820/multi-worker:latest -t jim123820/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jim123820/multi-server:latest -t jim123820/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jim123820/multi-worker:latest -t jim123820/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-# docker push jim123820/multi-client:latest
-# docker push jim123820/multi-server:latest
-# docker push jim123820/multi-worker:latest
+docker push jim123820/multi-client:latest
+docker push jim123820/multi-server:latest
+docker push jim123820/multi-worker:latest
 
-# docker push jim123820/multi-client:$SHA
-# docker push jim123820/multi-server:$SHA
-# docker push jim123820/multi-worker:$SHA
+docker push jim123820/multi-client:$SHA
+docker push jim123820/multi-server:$SHA
+docker push jim123820/multi-worker:$SHA
 
 # kubectl apply -f k8s
 # kubectl set image deployments/server-deployment server=jim123820/multi-server:$SHA
