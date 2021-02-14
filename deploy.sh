bff311593b25ee7f3887555264ea2751cb84c0e7
YYSU <jim123820@gmail.com>
2021-02-14 12:07:43 +0000

    Modify deploy sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push jim123820/multi-client:$SHA
 docker push jim123820/multi-server:$SHA
 docker push jim123820/multi-worker:$SHA
 
-# kubectl apply -f k8s
-# kubectl set image deployments/server-deployment server=jim123820/multi-server:$SHA
-# kubectl set image deployments/client-deployment client=jim123820/multi-client:$SHA
-# kubectl set image deployments/worker-deployment worker=jim123820/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jim123820/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jim123820/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jim123820/multi-worker:$SHA
